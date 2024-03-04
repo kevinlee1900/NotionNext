5dac5bf499740ed99cb2ac75dc7e3d8183d6f260
ariotrio <39135491+kevinlee1900@users.noreply.github.com>
2024-03-04 23:07:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '李觉吾', // 您的昵称 例如 觉吾
   BIO: process.env.NEXT_PUBLIC_BIO || '敬知识人性，畏客观规律', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jike.city/ariotrio', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '内容运营, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'ariotrio@qq.com', // 邮箱地址 例如mail@tangly1024.com
