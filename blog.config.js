ff148c05c1e3b5ef69b74930425513423ff64ab7
ariotrio <39135491+kevinlee1900@users.noreply.github.com>
2024-03-05 13:27:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,8 +17,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '觉吾', // 您的昵称 例如 觉吾
-  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '李觉吾', // 您的昵称 例如 觉吾
+  BIO: process.env.NEXT_PUBLIC_BIO || 'https://jike.city/ariotrio', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jike.city/ariotrio', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '敬知识人性,畏客观规律', // 网站关键词 英文逗号隔开
 
